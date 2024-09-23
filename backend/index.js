c4994e56c5ae3eedc21ff2d2163871a691fb29a5
Yogesh kumar <yogeshkumar12112003@gmail.com>
2024-09-23 22:36:51 +0530

    Added backend folder and code

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+
+const express = require('express')
+const cors = require ('cors')
+require('dotenv').config()
+const connectDB = require('./config/db')
+const router = require('./routes')
+
+const app = express()
+app.use(cors()) -  
+app.use(express.json())
+
+app.use("/api",router)
+    
+const PORT = 8080 || Process.env.PORT 
+
+connectDB().then(()=>{     
+    app.listen(PORT,()=>{
+        console.log("connected to DB")
+        console.log("server is running") 
+        })
+})
